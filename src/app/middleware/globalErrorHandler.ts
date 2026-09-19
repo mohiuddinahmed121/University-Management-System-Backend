a99a7aa7f6d2a2f68b74d36b5e4be762d8d0b0c6
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-09-19 12:18:47 +0600

    some issue fixed

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -10,9 +10,9 @@ export const globalErrorHandler = async (
    res: Response,
    _next: NextFunction,
 ) => {
-   if (config.node_env === "development") {
-      console.log("Error from Global Error Handler", err);
-   }
+   // if (config.node_env === "development") {
+   console.log("Error from Global Error Handler", err);
+   // }
 
    let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
    let errorMessage = "Something went wrong";
